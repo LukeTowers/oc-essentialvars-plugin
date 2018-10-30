5cd88fddad031b5e60c213bab898c6da39f940da
Luke Towers <luke@luketowers.ca>
2018-10-30 14:21:35 -0600

    Fixed race condition that caused the system module translations to break

--- Plugin.php
+++ Plugin.php
@@ -1,5 +1,6 @@
 <?php namespace LukeTowers\EssentialVars;
 
+use App;
 use Lang;
 use View;
 use Event;
@@ -34,28 +35,30 @@ class Plugin extends PluginBase
      */
      public function boot()
      {
-         $appVars = [
-             'url'         => url('/'),
-             'logo'        => BrandSetting::getLogo() ?: url('/modules/backend/assets/images/october-logo.svg'),
-             'name'        => BrandSetting::get('app_name'),
-             'debug'       => Config::get('app.debug', false),
-             'description' => BrandSetting::get('app_tagline'),
-         ];
+         App::before(function () {
+             $appVars = [
+                 'url'         => url('/'),
+                 'logo'        => BrandSetting::getLogo() ?: url('/modules/backend/assets/images/october-logo.svg'),
+                 'name'        => BrandSetting::get('app_name'),
+                 'debug'       => Config::get('app.debug', false),
+                 'description' => BrandSetting::get('app_tagline'),
+             ];
 
-         // Share the variables with the mail template system
-         View::share('app_url', $appVars['url']);
-         View::share('app_logo', $appVars['logo']);
-         View::share('app_name', $appVars['name']);
-         View::share('app_debug', $appVars['debug']);
-         View::share('app_description', $appVars['description']);
+             // Share the variables with the mail template system
+             View::share('app_url', $appVars['url']);
+             View::share('app_logo', $appVars['logo']);
+             View::share('app_name', $appVars['name']);
+             View::share('app_debug', $appVars['debug']);
+             View::share('app_description', $appVars['description']);
 
-         // Share the variables with the CMS template system
-         Event::listen('cms.page.beforeDisplay', function ($controller, $url, $page) use ($appVars) {
-             $controller->vars['app_url']         = $appVars['url'];
-             $controller->vars['app_logo']        = $appVars['logo'];
-             $controller->vars['app_name']        = $appVars['name'];
-             $controller->vars['app_debug']       = $appVars['debug'];
-             $controller->vars['app_description'] = $appVars['description'];
+             // Share the variables with the CMS template system
+             Event::listen('cms.page.beforeDisplay', function ($controller, $url, $page) use ($appVars) {
+                 $controller->vars['app_url']         = $appVars['url'];
+                 $controller->vars['app_logo']        = $appVars['logo'];
+                 $controller->vars['app_name']        = $appVars['name'];
+                 $controller->vars['app_debug']       = $appVars['debug'];
+                 $controller->vars['app_description'] = $appVars['description'];
+             });
          });
      }
 }
